400ce170cc8c2633ed0eb669bc4fb0d9f09a43f1
TraKhanhDuy <duytkce181326@fpt.edu.vn>
2025-10-29 23:35:15 +0700

    fix login

---
+++ Hospital.Common/AuthConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Common
+{
+    public static class AuthConfig
+    {
+        public const string CookieName = ".Hospital.SharedAuth";
+        public const string LoginPath = "/Account/Login";
+        public const string AccessDeniedPath = "/Account/AccessDenied";
+        public const string AppName = "HospitalAuthShared";
+        public static readonly string KeysPath = @"D:\HospitalSharedKeys";
+    }
+}
--- HospitalRazor/Pages/Doctors/Dashboard.cshtml.cs
+++ HospitalRazor/Pages/Doctors/Dashboard.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HospitalRazor.Pages.Doctors
 {
+    [Authorize(Roles = "DOCTOR")]
     public class DashboardModel : PageModel
     {
         public string DoctorName { get; set; } 
