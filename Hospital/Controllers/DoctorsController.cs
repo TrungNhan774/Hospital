0d48a2a76d0eca57b886841c88eb4f5757f5ee14
TraKhanhDuy <duytkce181326@fpt.edu.vn>
2025-11-03 17:00:20 +0700

    fix file

--- Hospital/Controllers/DoctorsController.cs
+++ Hospital/Controllers/DoctorsController.cs
@@ -8,7 +8,7 @@ using System;
 using System.Threading.Tasks;
 using X.PagedList.Extensions;
 
-namespace Hospital.Controllers.Admin
+namespace Hospital.Controllers
 {
     [Route("Admin/[controller]")]
     [Authorize(Roles = "ADMIN")]
